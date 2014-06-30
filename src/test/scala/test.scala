7424a20162853d61c82fe851b6ef96c4906fb692
joel <joel@joels-MacBook-Pro.local>
2014-06-30 18:31:25 +0100

    updated tests

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -3,6 +3,7 @@
  */
 
 import org.scalatest.FunSuite
+import org.scalatest.Matchers._
 
 class test extends FunSuite {
   test("test 2 expected values are equal") {
@@ -11,5 +12,11 @@ class test extends FunSuite {
 
   test("test 2 values are different and fail") {
     assert(2 === 3)
+  }
+
+  test("test output is Hello, World!") {
+    val s = "Hello, World!"
+    s should startWith("Hello,")
+    s should endWith("World!")
   }
 }
